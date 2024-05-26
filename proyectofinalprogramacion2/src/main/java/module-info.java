f3cde219579163511905270bc42f304e678daed0
JPabloLG <130943755+JPabloLG@users.noreply.github.com>
2024-05-26 12:44:40 -0500

    Creación de proyectos

---
+++ proyectofinalprogramacion2/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module co.edu.uniquindio.proyectofinalp2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens co.edu.uniquindio.proyectofinalp2 to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinalp2;
+}
